7a38c300b97e5a28a83658151083958034868e13
Vickopiyo <victor.okoth@student.moringaschool.com>
2022-09-07 14:55:33 +0300

    Adds: location of HQ

--- src/components/Contacts.js
+++ src/components/Contacts.js
@@ -3,8 +3,7 @@ import React from 'react'
 const Contacts = () => {
   return (
     <div>
-     <p className='discount-desc'>Tamaduni Dishes main offices are located in Ambalal buliding,3 floor Digo Road Mombasa</p>
-
+     <p className='discount-desc'>Tamaduni Dishes main offices are located in Ambalal buliding,Third floor Digo Road Mombasa CBD</p>
      <h3>Connect with us on :</h3>
      <p>Tel : +254701341969/+254731008628</p>
      <p>Email : management@tamadunidishes.com </p>
