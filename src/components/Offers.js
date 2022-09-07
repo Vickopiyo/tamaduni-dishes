3d0ec8b3f79e439748ba502e4422b50c60ea51aa
Vickopiyo <victor.okoth@student.moringaschool.com>
2022-09-07 14:51:35 +0300

    Adds : Offers & discount

--- src/components/Offers.js
+++ src/components/Offers.js
@@ -4,7 +4,6 @@ const Offers = () => {
   return (
     <div>
        <p className='discount-desc'>Every Friday we offer 20% Discount on every meal worth above ksh 2000.Thank you for your loyalty!</p>
-       
     </div>
   )
 }
