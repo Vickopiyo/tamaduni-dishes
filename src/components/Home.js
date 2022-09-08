6efe6b09aedb34bc17e41fc81e8429f87f924579
Vickopiyo <victor.okoth@student.moringaschool.com>
2022-09-08 15:36:53 +0300

    Styles buttons in the home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -7,10 +7,23 @@ import mkunga from "../images/mkunga.jpg"
 import pilau from "../images/pilau.jpg"
 import mkwaju from "../images/tamarindjuice.jpg"
 import {FaFacebook, FaInstagram, FaTwitter} from "react-icons/fa"
-import React from 'react'
+import React, {useState} from 'react'
 import NewComment from "./NewComment"
 
 const Home = () => {
+  const [buttonText, setButtonText] = useState('Order Now');
+  const [buttonText2, setButtonText2] = useState('Order Now');
+  const [buttonText3, setButtonText3] = useState('Order Now');
+
+  function handleClick() {
+    setButtonText('Ordered!');
+  }
+  function handleClick2() {
+    setButtonText2('Ordered!');
+  }
+  function handleClick3() {
+    setButtonText3('Ordered!');
+  }
   return (
     <div className="home_section">
       <h1 className="header">Tamaduni Dishes</h1>
@@ -28,21 +41,21 @@ const Home = () => {
           <h2>FoodType:Pilau</h2>
           <h2>Price : <span>Ksh 750</span></h2>
           <NewComment />
-          <button className="order-button">Order Now</button>
+          <button className="order-button" onClick={handleClick}>{buttonText}</button>
         </div>
         <div className="food-card">
           <img  src={kuku} alt="kuku"/>
           <h2>FoodType: kuku kienyeji</h2>
           <h2>Price : <span>Ksh 900</span></h2>
           <NewComment />
-          <button className="order-button">Order Now</button>
+          <button className="order-button" onClick={handleClick2}>{buttonText2}</button>
         </div>
         <div className="food-card">
           <img  src={lobster} alt="kuku"/>
           <h2>FoodType: Pweza</h2>
           <h2>Price : <span>Ksh 600</span></h2>
           <NewComment />
-          <button className="order-button">Order Now</button>
+          <button className="order-button" onClick={handleClick3}>{buttonText3}</button>
         </div>
         {/* <div className="food-card">
           <img  src={mkunga} alt="mkunga"/>
